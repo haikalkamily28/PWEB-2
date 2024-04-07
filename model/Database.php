4cb3378a526b9c7d259a2dcdf0c7a9a59e4776d9
haikalkamily28 <1112talent@gmail.com>
2024-04-07 21:59:47 +0700

    p

---
+++ model/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+Class Contact{ 
+
+    public $id_user;
+    public $username;
+    public $email;
+    public $nomor_hp;
+    public $password;
+    
+    static function select(){
+  
+    }
+    static function insert(){
+  
+    }
+    static function update(){
+  
+    }
+    static function delete(){
+  
+    }
+  }
